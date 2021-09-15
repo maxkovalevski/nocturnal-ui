6e46465fec3d39acffe13d7a1953da641581d228
kowalevski <me@kowalevski.com>
2021-09-15 23:44:04 +0200

    rewrote PostBanner styles from css modules to js objects (temp)

--- packages/nocturnal-ui-react/src/components/PostBanner/PostBanner.tsx
+++ packages/nocturnal-ui-react/src/components/PostBanner/PostBanner.tsx
@@ -1,6 +1,10 @@
 import React, { FC } from "react";
 
-import * as styles from "./post-banner.module.css";
+// css was rewritten to JS's objects because this component as MDX component
+// in my blog project MDX works by bundleMDX which uses esbuild under the hood
+// by default esbuild doesn't support css modules 
+// so for now styles are written as objects instead of css modules
+// import * as styles from "./post-banner.module.css";
 
 export interface PostBannerProps {
   title: React.ReactNode;
@@ -19,6 +23,73 @@ export interface PostBannerProps {
   status?: "initial" | "success" | "error";
 }
 
+const bannerStyle = {
+  padding: '20px 30px 30px 30px',
+  borderRadius: '4px',
+  marginBottom: '20px',
+  background: '#212123',
+  border: '2px dashed',
+  borderColor: '#759cc0',
+}
+
+const titleStyle = {
+  display: 'inline-block',
+  marginBottom: '1.2rem',
+  fontFamily: 'Roboto Mono, monospace',
+  fontWeight: 700,
+  borderBottomWidth: '2px',
+  //borderBottomStyle: 'solid',
+  borderBottomColor: '#484848',
+  //marginBottom: '2rem',
+  borderStyle: 'solid',
+}
+
+const formStyle = {
+  marginBottom: '15px',
+}
+
+const inputStyle = {
+  background: '#f4f4f4',
+  outline: 'none',
+  border: '1px solid #b5b6b7',
+  padding: '18px 14px',
+  borderRadius: '4px',
+  maxWidth: '360px',
+  width: '100%',
+  /* color: #b5b6b7; */
+  color: '#333',
+
+  fontSize: '1.3rem',
+}
+
+const btnStyle = {
+  marginLeft: '20px',
+  marginBottom: '5px',
+  /* background: none; */
+  outline: 'none',
+  //border: 'none',
+  fontFamily: "Roboto Mono",
+  /* border-bottom: 2px solid #484848; */
+  //padding: 0,
+
+  background: '#387cc8',
+  padding: '0 25px',
+  height: '62px',
+  borderRadius: '4px',
+  fontWeight: 700,
+  border: '1px solid #1c1d1e',
+
+  fontSize: '1.3rem',
+}
+
+const resultStyle = {
+  fontSize: '1.2rem !important',
+  display: 'flex',
+  flexWrap: 'wrap',
+  alignItems: 'flex-start',
+} as React.CSSProperties;
+
+
 export const PostBanner: FC<PostBannerProps> = ({
   title,
   content,
@@ -34,22 +105,22 @@ export const PostBanner: FC<PostBannerProps> = ({
   status = "initial",
 }) => {
   return (
-    <div className={styles.banner}>
-      <h3>{title}</h3>
-      <div className={styles.content}>{content}</div>
-      <form className={styles.form} onSubmit={onSubmit}>
+    <div style={bannerStyle}>
+      <h3 style={titleStyle}>{title}</h3>
+      <div>{content}</div>
+      <form style={formStyle} onSubmit={onSubmit}>
         <input
-          className={styles.input}
+          style={inputStyle}
           type={inputType}
           name={inputName}
           placeholder={inputPlaceholder}
           onChange={onChange}
           value={inputValue}
         />
-        <button className={styles.btn}>{btnTitle}</button>
+        <button style={btnStyle}>{btnTitle}</button>
       </form>
       {status !== "initial" && (
-        <div className={styles.result}>
+        <div style={resultStyle}>
           {status === "success" && successView}
           {status === "error" && errorView}
         </div>
--- packages/nocturnal-ui-react/src/components/PostBanner/post-banner.stories.tsx
+++ packages/nocturnal-ui-react/src/components/PostBanner/post-banner.stories.tsx
@@ -1,4 +1,4 @@
-import React, { FC } from "react";
+import React, { FC, useState } from "react";
 import { Story, Meta } from "@storybook/react";
 
 import { PostBanner, PostBannerProps } from "./PostBanner";
@@ -11,26 +11,58 @@ export default {
   },
 } as Meta;
 
-const Template: Story<PostBannerProps> = (args) => <PostBanner {...args} />;
+const Template: Story<PostBannerProps> = (args) => {
+  const [val, setVal] = useState<string>("");
+  const [count, setCount] = useState<number>(0);
+  const [status, setStatus] = useState<"initial" | "success" | "error">();
+
+  const onSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+
+    if (count % 2 == 0) {
+      setStatus('success');
+    } else {
+      setStatus('error');
+    }
+
+    setCount((prev) => prev + 1);
+  }
+
+  const onChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setVal(e.currentTarget.value);
+  }
+
+  return <div style={{maxWidth: '900px'}}>
+    <PostBanner 
+      {...args} 
+      status={status} 
+      onSubmit={onSubmit} 
+      inputValue={val} 
+      onChange={onChange} 
+    />
+  </div>
+};
 
 export const Main = Template.bind({});
 Main.args = {
   title: "Sign Up To...",
   content: (
-    <div>
+    <p>
       Amet do quis proident ut culpa tempor veniam aute aliqua consequat ea sint
       nostrud deserunt. Aliquip laborum pariatur est esse. Quis fugiat amet esse
       Lorem incididunt fugiat enim occaecat laboris.
-    </div>
+    </p>
   ),
-  btnTitle: "Sign Up",
-  successView: () => <div>Congratulations!</div>,
-  errorView: () => <div>Something goes wrong</div>,
-  onSubmit: (e: React.FormEvent<HTMLFormElement>) => {},
-  onChange: (e: React.ChangeEvent<HTMLInputElement>) => {},
-  inputName: "username",
-  inputType: "text",
-  inputPlaceholder: "Type username",
+  btnTitle: "Subscribe",
+  successView: <div>
+    <span>Please go confirm your subscription!</span>
+  </div>,
+  errorView: <div>
+    <span>Oops, Something went wrong! Try again.</span>
+  </div>,
+  inputName: "email",
+  inputType: "email",
+  inputPlaceholder: "Email address",
   inputValue: "",
   status: "initial",
 };
